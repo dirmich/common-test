80b402efd9055288ef14745d8a45dd88446621df
David, Shin <dirmich@gmail.com>
2024-08-09 17:41:27 +0900

    a

--- bulktest.js
+++ bulktest.js
@@ -1,3 +1,4 @@
+import { parallel } from 'radash'
 import mongo, { Model } from './testmongo.js'
 
 function Defer(func) {
@@ -28,7 +29,7 @@ const start = 0
 // const end = 16777215
 const end = 1000000
 const ichunkSize = 100000
-
+const NUM_PROC = 3
 class DBMan {
   constructor(chuckSize = ichunkSize) {
     this.CHUNKSIZE = chuckSize
@@ -40,7 +41,7 @@ class DBMan {
     this.dbloaded = true
   }
 
-  async chunkProc(start, end, fn) {
+  async chunkProc1(start, end, fn) {
     return new Promise(async (resolve, reject) => {
       let currStart = start
       let currEnd =
@@ -71,12 +72,51 @@ class DBMan {
       resolve(true)
     })
   }
+  async chunkProc(start, end, fn) {
+    return new Promise(async (resolve, reject) => {
+      let currStart = start
+      let currEnd =
+        start + this.CHUNKSIZE - 1 > end ? end : start + this.CHUNKSIZE - 1
+      let currTotal = 0
+      const total = end - start + 1
+      let idx = 0
+      const proc = []
+      while (currStart < end) {
+        currTotal += currEnd - currStart + 1
+        try {
+          proc.push({ currStart, currEnd, total, currTotal, idx })
+          idx++
+          currStart = currEnd + 1
+          currEnd =
+            currStart + this.CHUNKSIZE - 1 > end
+              ? end
+              : currStart + this.CHUNKSIZE - 1
+        } catch (e) {
+          reject(e)
+        }
+      }
+      parallel(NUM_PROC, proc.reverse(), async (item) => {
+        if (fn) {
+          await fn(
+            item.currStart,
+            item.currEnd,
+            item.total,
+            item.currTotal,
+            item.idx
+          )
+        }
+      })
+        .then((v) => resolve(true))
+        .catch((e) => reject(e))
+    })
+  }
 
   async test(start, end) {
     if (!this.dbloaded) await this.initDB()
-    return this.remove('', start, end, async (p) => {
+    const ret = this.insert('', start, end, async (p) => {
       console.log('#', p)
     })
+    return ret
   }
   async insert(oui, start, end, onUpdate) {
     return this.chunkProc(start, end, async (cs, ce, t, ct, idx) => {
@@ -141,3 +181,28 @@ deferred.promise
   .then((a) => console.log('defer end', a))
   .catch((e) => console.log('defer err', e))
 deferred.trigger()
+
+//
+//  chunkproc
+// 1000000 insert : proc: 44.517s
+// 1000000 remove : proc: 34.134s
+// 16777215 insert : proc: 12:49.943
+// 16777215 remove : proc: 11:26.120
+
+//  chunkproc2  p3
+// 1000000 insert : proc: 36.711s
+// 1000000 remove : proc: 23.713s
+// 16777215 insert : proc: 10:42.307
+// 16777215 remove : proc: 6:55.989
+
+//  chunkproc2  p4
+// 1000000 insert : proc: 36.301s
+// 1000000 remove : proc: 21.819s
+// 16777215 insert : proc: 10:59.915
+// 16777215 remove : proc: 6:49.224
+
+//  chunkproc2  p5
+// 1000000 insert : proc: 37.668s
+// 1000000 remove : proc: 21.609s
+// 16777215 insert : proc: 11:15.023
+// 16777215 remove : proc: 7:34.055
--- radashtest.js
+++ radashtest.js
@@ -2,6 +2,15 @@ import { parallel } from 'radash'
 
 const userIds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const users = await parallel(5, userIds, async (uid) => {
+const users = await parallel(5, userIds.reverse(), async (uid) => {
   console.log('->', uid)
+  return uid
 })
+  .then((v) => {
+    console.log('end', v)
+  })
+  .catch((e) => {
+    console.log('E]', e)
+  })
+
+console.log('finish')
