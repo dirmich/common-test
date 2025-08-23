28d34067add8fcc4a3a8fcb3954663d7ab78aab4
David, Shin <dirmich@gmail.com>
2025-08-23 14:20:00 +0900

    indexbuf test

--- hybe.js
+++ hybe.js
@@ -123,142 +123,161 @@ const hasFile = (url) => {
 
 // console.log(hasFile("url/datas"), hasFile("url/datas/canvas.png"))
 
-// export default class IndexBuffer {
-//   constructor(maxInstance) {
-//     this.maxInstance = maxInstance
-//     this.ptr = 0 // 검색 포인터 초기화
-//     this._init()
-//   }
+export default class IndexBuffer {
+  constructor(maxInstance) {
+    this.maxInstance = maxInstance
+    this.ptr = 0 // 검색 포인터 초기화
+    this._init()
+  }
 
-//   _init() {
-//     // 모든 인덱스를 사용 가능 상태(0)로 초기화
-//     this.freeIndices = new Array(this.maxInstance).fill(0)
-//     this.nextIndex = 0 // 다음 할당 시작 위치 (현재는 최적화 용도)
-//     this.count = 0 // 현재 사용 중인 인덱스 개수
-//     this.ptr = 0 // 검색 포인터 리셋
-//     console.log("init", this.freeIndices, this.count)
-//   }
+  _init() {
+    // 모든 인덱스를 사용 가능 상태(0)로 초기화
+    this.freeIndices = new Array(this.maxInstance).fill(0)
+    this.nextIndex = 0 // 다음 할당 시작 위치 (현재는 최적화 용도)
+    this.count = 0 // 현재 사용 중인 인덱스 개수
+    this.ptr = 0 // 검색 포인터 리셋
+    console.log("init", this.freeIndices, this.count)
+  }
 
-//   init() {
-//     this._init()
-//   }
+  init() {
+    this._init()
+  }
 
-//   clear() {
-//     this._init()
-//   }
+  clear() {
+    this._init()
+  }
 
-//   /**
-//    * 사용 가능한 인덱스를 가져옵니다 (현재 위치부터 검색)
-//    * @param {boolean} [fromStart=false] 처음부터 검색할지 여부
-//    * @returns {number} 사용 가능한 인덱스 또는 -1 (없을 경우)
-//    */
-//   getIndex(fromStart = false) {
-//     if (fromStart) {
-//       this.ptr = 0 // 검색 포인터를 맨 앞으로 리셋
-//     }
+  /**
+   * 사용 가능한 인덱스를 가져옵니다 (현재 위치부터 검색)
+   * @param {boolean} [fromStart=false] 처음부터 검색할지 여부
+   * @returns {number} 사용 가능한 인덱스 또는 -1 (없을 경우)
+   */
+  getIndex(fromStart = false) {
+    if (fromStart) {
+      this.ptr = 0 // 검색 포인터를 맨 앞으로 리셋
+    }
 
-//     // 현재 포인터 위치부터 사용 가능한 인덱스(값이 0인 곳)를 찾습니다
-//     while (this.ptr < this.maxInstance && this.freeIndices[this.ptr] === 0) {
-//       this.ptr++
-//     }
+    // 현재 포인터 위치부터 사용 가능한 인덱스(값이 0인 곳)를 찾습니다
+    while (this.ptr < this.maxInstance && this.freeIndices[this.ptr] === 0) {
+      this.ptr++
+    }
 
-//     const result = this.ptr < this.maxInstance ? this.ptr : -1
+    const result = this.ptr < this.maxInstance ? this.ptr : -1
 
-//     // 사용 가능한 인덱스를 찾았다면, 포인터를 다음 후보 위치로 이동
-//     if (result !== -1) {
-//       this.ptr++
-//     }
+    // 사용 가능한 인덱스를 찾았다면, 포인터를 다음 후보 위치로 이동
+    if (result !== -1) {
+      this.ptr++
+    }
 
-//     // console.log("getindex", result, this.ptr)
-//     return result
-//   }
+    // console.log("getindex", result, this.ptr)
+    return result
+  }
 
-//   /**
-//    * 새로운 인덱스를 할당합니다
-//    * @returns {number} 새로 할당된 인덱스 또는 -1 (풀이 꽉 찼을 경우)
-//    */
-//   add() {
-//     // const idx = this.getIndex(true) // 사용 가능한 인덱스를 찾습니다
-//     while (this.ptr < this.maxInstance && this.freeIndices[this.ptr] !== 0) {
-//       this.ptr++
-//     }
-//     const idx = this.ptr
-//     if (idx === -1) {
-//       return -1 // 사용 가능한 인덱스가 없음
-//     }
+  /**
+   * 새로운 인덱스를 할당합니다
+   * @returns {number} 새로 할당된 인덱스 또는 -1 (풀이 꽉 찼을 경우)
+   */
+  add() {
+    this.ptr = 0
+    // const idx = this.getIndex(true) // 사용 가능한 인덱스를 찾습니다
+    while (this.ptr < this.maxInstance && this.freeIndices[this.ptr] !== 0) {
+      this.ptr++
+    }
+    const idx = this.ptr < this.maxInstance ? this.ptr : -1
+    if (idx === -1) {
+      return -1 // 사용 가능한 인덱스가 없음
+    }
 
-//     this.freeIndices[idx] = 1 // 인덱스를 사용 중으로 표시
-//     this.count++ // 사용 중인 인덱스 개수 증가
-//     // console.log("add", idx, this.count)
-//     return idx
-//   }
+    this.freeIndices[idx] = 1 // 인덱스를 사용 중으로 표시
+    this.count++ // 사용 중인 인덱스 개수 증가
+    // console.log("add", idx, this.count)
+    return idx
+  }
 
-//   /**
-//    * 단일 인덱스를 해제합니다
-//    * @param {number} i 해제할 인덱스
-//    */
-//   remove(i) {
-//     // 인덱스 범위 검증
-//     if (i < 0 || i >= this.maxInstance) {
-//       return // 유효하지 않은 인덱스는 무시
-//     }
+  /**
+   * 단일 인덱스를 해제합니다
+   * @param {number} i 해제할 인덱스
+   */
+  remove(i) {
+    // 인덱스 범위 검증
+    if (i < 0 || i >= this.maxInstance) {
+      return // 유효하지 않은 인덱스는 무시
+    }
 
-//     // 인덱스가 사용 중이라면 해제합니다
-//     if (this.freeIndices[i] === 1) {
-//       this.freeIndices[i] = 0 // 인덱스를 사용 가능으로 표시
-//       this.count-- // 사용 중인 인덱스 개수 감소
+    // 인덱스가 사용 중이라면 해제합니다
+    if (this.freeIndices[i] === 1) {
+      this.freeIndices[i] = 0 // 인덱스를 사용 가능으로 표시
+      this.count-- // 사용 중인 인덱스 개수 감소
 
-//       // nextIndex를 최소한의 해제된 인덱스로 업데이트 (재사용 우선 순위)
-//       this.nextIndex = Math.min(i, this.nextIndex)
-//       // console.log("remove", i, this.count)
-//     } else {
-//       console.log("remove fail", i, this.count)
-//     }
-//   }
+      // nextIndex를 최소한의 해제된 인덱스로 업데이트 (재사용 우선 순위)
+      this.nextIndex = Math.min(i, this.nextIndex)
+      // console.log("remove", i, this.count)
+    } else {
+      console.log("remove fail", i, this.count)
+    }
+  }
 
-//   /**
-//    * 여러 인덱스를 일괄 해제합니다
-//    * @param {number[]} ids 해제할 인덱스 배열
-//    */
-//   removes(ids) {
-//     let validFrees = 0 // 성공적으로 해제된 유효 인덱스 개수
+  /**
+   * 여러 인덱스를 일괄 해제합니다
+   * @param {number[]} ids 해제할 인덱스 배열
+   */
+  removes(ids) {
+    let validFrees = 0 // 성공적으로 해제된 유효 인덱스 개수
 
-//     for (const i of ids) {
-//       // 유효하지 않은 인덱스는 건너뜁니다
-//       if (i < 0 || i >= this.maxInstance) {
-//         continue
-//       }
+    for (const i of ids) {
+      // 유효하지 않은 인덱스는 건너뜁니다
+      if (i < 0 || i >= this.maxInstance) {
+        continue
+      }
 
-//       // 인덱스가 사용 중이라면 해제합니다
-//       if (this.freeIndices[i] === 1) {
-//         this.freeIndices[i] = 0 // 인덱스 해제
-//         validFrees++
-//       }
-//     }
+      // 인덱스가 사용 중이라면 해제합니다
+      if (this.freeIndices[i] === 1) {
+        this.freeIndices[i] = 0 // 인덱스 해제
+        validFrees++
+      }
+    }
 
-//     this.count -= validFrees // 사용 중인 인덱스 개수 감소 (실제 해제된 개수만큼)
+    this.count -= validFrees // 사용 중인 인덱스 개수 감소 (실제 해제된 개수만큼)
 
-//     // nextIndex를 최소한의 해제된 인덱스로 업데이트 (재사용 우선 순위)
-//     if (ids.length > 0) {
-//       const validIds = ids.filter((id) => id >= 0 && id < this.maxInstance)
-//       if (validIds.length > 0) {
-//         const minId = Math.min(...validIds)
-//         this.nextIndex = Math.min(minId, this.nextIndex)
-//       }
-//     }
-//   }
-// }
+    // nextIndex를 최소한의 해제된 인덱스로 업데이트 (재사용 우선 순위)
+    if (ids.length > 0) {
+      const validIds = ids.filter((id) => id >= 0 && id < this.maxInstance)
+      if (validIds.length > 0) {
+        const minId = Math.min(...validIds)
+        this.nextIndex = Math.min(minId, this.nextIndex)
+      }
+    }
+  }
+  toString() {
+    const buf = []
+    for (let i = 0; i < this.count; i++) {
+      const idx = this.getIndex(i === 0)
+      if (idx >= 0) buf.push(idx)
+    }
+    return `${this.count}:${this.freeIndices} / ${buf.join(", ")}`
+  }
+}
 
 const buf = new IndexBuffer(5)
-console.log(buf)
+console.log(buf.toString())
 buf.add()
 buf.add()
 buf.add()
-console.log(buf)
+console.log(buf.toString())
 buf.remove(0)
 buf.remove(2)
-console.log(buf)
+console.log(buf.toString())
 buf.add()
 buf.add()
 buf.add()
-console.log(buf)
+console.log(buf.toString())
+buf.removes([1, 3, 5])
+console.log(buf.toString())
+buf.add()
+buf.add()
+buf.add()
+buf.add()
+buf.add()
+buf.add()
+buf.add()
+console.log(buf.toString())
